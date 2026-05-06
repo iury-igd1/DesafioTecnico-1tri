fc84e1b6cc8cb860454bb31a451520b882031526
iury-igd1 <iurygoncalvessrs1@gmail.com>
2026-05-06 10:26:37 -0300

    commitDesafio

---
+++ README.md
@@ -0,0 +1,6 @@
+Integrantes da dupla:
+
+* Caroline Maciel de Ávila Rosa;
+* Iury Gonçalves de Souza.
+
+34 DS
---
+++ desafio/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static int calcularOfensiva (int forca, int agilidade) {
+        return forca + agilidade;
+    }
+
+    static void Main(string[] args) {
+        string nome1 = "Hulk";
+        int forca1 = 8;
+        int agilidade1 = 5;
+        int inteligencia1 = 3;
+        int vida1 = 20;
+
+        string nome2 = "Homem de Ferro";
+        int forca2 = 4;
+        int agilidade2 = 6;
+        int inteligencia2 = 9;
+        int vida2 = 15;
+
+        Console.WriteLine("- BATALHA -");
+
+        Console.WriteLine($"{nome1} - Força: {forca1}, Agilidade: {agilidade1}, Inteligência: {inteligencia1}, Vida: {vida1}");
+        Console.WriteLine("VS");
+        Console.WriteLine($"{nome2} - Força: {forca2}, Agilidade: {agilidade2}, Inteligência: {inteligencia2}, Vida: {vida2}");
+        Console.WriteLine("\nBatalha OFENSIVA: Ganha quem tiver maior força + agilidade!");
+        
+        Console.Write($"Determinando o vencedor em ");
+        for (int i = 3; i >= 1; i--) {
+            Console.Write($"{i}... ");
+        }
+
+        int ofensiva1 = calcularOfensiva(forca1, agilidade1);
+        int ofensiva2 = calcularOfensiva(forca2, agilidade2);
+
+        if (ofensiva1 > ofensiva2) {
+            Console.WriteLine($"\n\n{nome1} foi o vencedor da batalha com {ofensiva1} pontos! {nome2} fez apenas {ofensiva2} pontos.");
+        }
+        else if (ofensiva2 > ofensiva1) {
+            Console.WriteLine($"\n\n{nome2} foi o vencedor da batalha com {ofensiva2} pontos! {nome1} fez apenas {ofensiva1} pontos.");
+        }
+        else {
+            Console.WriteLine($"\n\nEmpate entre os heróis! {ofensiva1} pontos para cada um.");
+        }
+    }
+}
